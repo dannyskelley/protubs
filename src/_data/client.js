d4a0ebcf928c10ccb502955856b28fd29f3189c5
Danny Skelley <dannyskelley@gmail.com>
2025-07-14 13:46:29 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: "Providence Properties, Realty & Management",
-    email: "Victoria@providencemichigan.com",
+    name: "A & A Outdoor LLC",
+    email: "Aandaoutdoorllc@gmail.com",
     phoneForTel: "810-531-6867",
     phoneFormatted: "810-531-6867",
     address: {
@@ -10,7 +10,7 @@ module.exports = {
         state: "MI",
         zip: "48195",
         country: "US",
-        mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2955.699858071792!2d-83.1980291!3d42.1994846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883b377a875bcc87%3A0x7aee19f28e9c0be3!2sProvidence%20Properties%2C%20Realty%20%26%20Management!5e0!3m2!1sen!2sus!4v1750338203909!5m2!1sen!2sus",
+        mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d376060.03206968214!2d-83.30923325!3d42.57718044999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa5c04c4ad6f2a3d5%3A0xa643d5c56ad54a24!2sA%20and%20A%20Outdoor%20LLC!5e0!3m2!1sen!2sus!4v1752513334459!5m2!1sen!2sus",
     },
     socials: {
         facebook: "https://www.facebook.com/Providencemichigan/",
@@ -22,3 +22,4 @@ module.exports = {
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
+
